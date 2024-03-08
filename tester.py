a98d1244f174ff3087ffc0dbce71ddc1c361f8ba
Corey_JWong <129793975+wongco-oregon@users.noreply.github.com>
2024-03-08 11:21:38 -0800

    Update tester.py

--- tester.py
+++ tester.py
@@ -3,6 +3,6 @@ import requests
 flask_app_url = 'http://127.0.0.1:5000'
 
 # Data to be sent with the request
-data1 = {'order_name': 'Yeji', 'order_email': 'corew531@gmail.com', 'order_total': '$123.45'}
+data1 = {'order_name': 'Jennifer', 'order_email': 'youremail@email.com', 'order_total': '$123.45'}
 response = requests.post(flask_app_url, data=data1)
 print("Response message:", response.text)
